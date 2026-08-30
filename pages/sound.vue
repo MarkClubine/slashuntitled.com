e94eb0092ba75c50e6ac5b2c9c96c6c790c4b69e
slash-untitled <work@slashuntitled.com>
2026-08-30 18:57:33 +0200

    Fix sound page: ssr:false + integrate with global player

--- pages/sound.vue
+++ pages/sound.vue
@@ -1,25 +1,21 @@
-<script setup>
-const site = useSite()
-const sounds = computed(() => site.sounds ?? [])
-const playing = ref(null)
+<script setup lang="ts">
+definePageMeta({ ssr: false })
 
-function toggle(filename) {
+const site = useSite()
+const sounds = computed(() => (site as any).sounds ?? [])
+const playing = useState<string | null>('playing')
+
+function toggle(filename: string) {
   playing.value = playing.value === filename ? null : filename
 }
 </script>
 
 <template>
   <section>
-    <audio
-      v-if="playing"
-      :src="`/sounds/${playing.value}`"
-      autoplay
-      @ended="playing.value = null"
-    />
     <ul class="list">
-      <li v-for="sound in sounds" :key="sound.file">
-        <button class="block w-fit text-left" @click="toggle(sound.file)">
-          {{ sound.name }}<span v-if="playing.value === sound.file"> ◼</span><span v-else> ▶</span>
+      <li v-for="sound in sounds" :key="(sound as any).file">
+        <button class="block w-fit text-left" @click="toggle((sound as any).file)">
+          {{ (sound as any).name }}<span v-if="playing === (sound as any).file"> ◼</span>
         </button>
       </li>
     </ul>
